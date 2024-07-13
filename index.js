6d6d374d299d142d96bb8e2357e8c108e3194627
javid shabin <javid.main.act@gmail.com>
2024-07-13 14:29:58 +0530

    install cors middleware

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors')
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors())
+app.use(express.json())
 
+// Connect Database
 async function main() {
   await mongoose.connect("mongodb+srv://javidprsnlact:t41sHOj6MIhD7umG@cluster0.ujfj8le.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 main().then(() => console.log("Connected")).catch((err) => console.log(err));
 
+// Create server
 app.listen(port, () => {
   console.log(`The server running in port: ${port}`);
 });
