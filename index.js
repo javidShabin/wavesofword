d584a6eba4c0fe4156139a9437993fafd0b5e270
javid shabin <javid.main.act@gmail.com>
2024-07-13 14:27:09 +0530

    Database connect with express throug mongoose. Mongoose install and confuig

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, ()=> {
-    console.log(`The server running in port: ${port}`)
-})
+async function main() {
+  await mongoose.connect("mongodb+srv://javidprsnlact:t41sHOj6MIhD7umG@cluster0.ujfj8le.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+}
+main().then(() => console.log("Connected")).catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`The server running in port: ${port}`);
+});
