3fb001e7624355009b6bbfa031a6f3a6ace829c0
javid shabin <javid.main.act@gmail.com>
2024-07-13 14:19:26 +0530

    Create a server with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Running")
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, ()=> {
+    console.log(`The server running in port: ${port}`)
+})
