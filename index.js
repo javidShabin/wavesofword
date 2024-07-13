77ffd65dcebd7b3fe4ee7a5a994ddb3fad6f2249
javid shabin <javid.main.act@gmail.com>
2024-07-13 15:00:34 +0530

    Add model for books and addschema for the model

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/books', bookRoute)
-app.use('/books', autherRoute)
+app.use('/authers', autherRoute)
 
 // Connect Database
 async function main() {
