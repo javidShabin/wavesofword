07a0f1c4b2088308a409f4f1434956179ab8ce46
javid shabin <javid.main.act@gmail.com>
2024-07-28 00:05:44 +0530

    install bcrypt from npm and protect user password using bcrypt module

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,21 @@
 const User = require('../models/userModel')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // Get all users
 const getAllUsers = async(req, res) => {
     const user = await User.find({})
+    res.json(user)
 }
 
 // Add user
 const addUser = async(req, res) => {
     const userData = req.body
-    const user = new User(userData)
+    const hash = bcrypt.hashSync(userData.password, saltRounds);
+    const user = new User({
+        ...userData,
+        password: hash
+    })
     await user.save()
     res.json(user)
 }
